a78815e0d60e9203e61eb38a98d1e87e18cbd898
Kaewmanee Marasri <64160234@go.buu.ac.th>
2022-04-13 16:31:37 +0700

    modified:   inventoryproject/settings.py

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import djang_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,4 +138,3 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
 
-django_heroku.settings(locals)
